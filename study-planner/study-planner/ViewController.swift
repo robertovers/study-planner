31fb6bf0c6c29a34f660ad04593a68d624750f99
Robert Overs <oversr14@gmail.com>
2021-01-25 09:55:50 +1100

    create storyboard objects

--- study-planner/study-planner/ViewController.swift
+++ study-planner/study-planner/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    @IBOutlet weak var studyButton: UIButton!
+    
+    @IBOutlet weak var loopButton: UIButton!
+    
+    @IBOutlet weak var sbButton: UIButton!
+    
+    @IBOutlet weak var lbButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func studyButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func loopButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sbButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func lbButtonPressed(_ sender: Any) {
+    }
+    
+    
 }
 
