e73f14b3f08d18322b2ab68be4453991d5b977c5
Robert Overs <oversr14@gmail.com>
2021-01-25 20:56:04 +1100

    add loop functionality to timer

--- study-planner/study-planner/Controllers/ViewController.swift
+++ study-planner/study-planner/Controllers/ViewController.swift
@@ -9,19 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let studyTime: Int = 25
+    let studyTime: Int = 10
     
-    let shortBreakTime: Int = 1
+    let shortBreakTime: Int = 2
     
-    let longBreakTime: Int = 20
+    let longBreakTime: Int = 8
     
-    var currentTime: Int = 25*60
+    var currentTime: Int = 0
     
-    var recentTime: Int = 25*60
+    var recentTime: Int = 0
+    
+    var recentMode: Int = 0
     
     var playing: Bool = false
     
     var timer = Timer()
+    
+    var inloop: Int = -1
+    
+    var breakCount: Int = 2
+    
+    var loopCount: Int = 1
+    
+    var wasInLoop: Bool = false
     
     @IBOutlet weak var playButton: UIButton!
     
@@ -48,13 +58,11 @@ class ViewController: UIViewController {
         
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         
-        timerLabel.text = convertSeconds(secs: currentTime)
+        timerLabel.text = convertSeconds(secs: studyTime)
     }
     
     func convertSeconds(secs: Int) -> String {
-
         let mins: Int = Int(round(Double(secs/60)))
-        
         var minsString: String
         
         if mins < 10 {
@@ -64,7 +72,6 @@ class ViewController: UIViewController {
         }
         
         let seconds: Int = secs % 60
-        
         var secondsString: String
         
         if seconds < 10 {
@@ -79,15 +86,30 @@ class ViewController: UIViewController {
     }
     
     @objc func timerAction() {
-        if playing {
+        if playing && inloop <= 0 {
             if currentTime == 0 {
                 playing = false
                 playButton.setImage(UIImage(systemName: "gobackward"), for: .normal)
             } else {
                 currentTime -= 1
             }
-            timerLabel.text = convertSeconds(secs: currentTime)
+        } else if playing {
+            if currentTime == 0 && ((inloop-1) % (breakCount*2+2) == 0) {
+                inloop -= 1
+                currentTime = longBreakTime*60
+                statusLabel.text = "Loop (Long Break)"
+            } else if currentTime == 0 && ((inloop % 2) == 1) {
+                inloop -= 1
+                currentTime = shortBreakTime*60
+                statusLabel.text = "Loop (Short Break)"
+            } else if currentTime == 0 {
+                inloop -= 1
+                currentTime = studyTime*60
+                statusLabel.text = "Loop (Study)"
+            }
+            currentTime -= 1
         }
+        timerLabel.text = convertSeconds(secs: currentTime)
     }
     
     @IBAction func playButtonPressed(_ sender: Any) {
@@ -99,13 +121,18 @@ class ViewController: UIViewController {
             playButton.setImage(UIImage(systemName: "pause"), for: .normal) // play
         } else if currentTime == 0 {
             playing = false
-            currentTime = recentTime
-            playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)  // pause
-            timerLabel.text = convertSeconds(secs: currentTime)
+            if inloop == 0 {
+                loopButtonPressed(self)
+            } else {
+                currentTime = recentTime
+                playButton.setImage(UIImage(systemName: "play.fill"), for: .normal)  // pause
+                timerLabel.text = convertSeconds(secs: currentTime)
+            }
         }
     }
     
     @IBAction func studyButtonPressed(_ sender: Any) {
+        inloop = -1
         if playing || currentTime == 0 {
             playButtonPressed(self)
         }
@@ -116,16 +143,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func loopButtonPressed(_ sender: Any) {
+        inloop = loopCount*(breakCount*2+2)-1
         if playing || currentTime == 0 {
             playButtonPressed(self)
         }
         currentTime = studyTime*60
         recentTime = studyTime*60
         timerLabel.text = convertSeconds(secs: currentTime)
-        statusLabel.text = "Loop"
+        statusLabel.text = "Loop (Study)"
     }
     
     @IBAction func sbButtonPressed(_ sender: Any) {
+        inloop = -1
         if playing || currentTime == 0 {
             playButtonPressed(self)
         }
@@ -136,6 +165,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func lbButtonPressed(_ sender: Any) {
+        inloop = -1
         if playing || currentTime == 0 {
             playButtonPressed(self)
         }
